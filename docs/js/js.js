b80a774643957f30099b47a3be19811ca50542f5
dt-tl <dan@truthlabs.com>
2016-12-13 16:27:12 -0600

    remove canvas - switch to css

--- docs/js/js.js
+++ docs/js/js.js
@@ -1,15 +1,4 @@
 $(document).ready(function() {
 
     var sweater = $("#sweater")[0];
-
-    drawPattern = function() {
-        var ctx = sweater.getContext("2d");
-        ctx.clearRect(0, 0, sweater.width, sweater.height);
-        var img = document.getElementById("pattern1");
-        var pat = ctx.createPattern(img, 'repeat');
-        ctx.rect(0, 0, 200, 280);
-        ctx.fillStyle = pat;
-        ctx.fill();
-    };
-    setTimeout(drawPattern, 500);
 });
