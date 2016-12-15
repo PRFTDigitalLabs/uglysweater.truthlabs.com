a6da0862238048e4e2cc549778e41ec698c2b418
dt-tl <dan@truthlabs.com>
2016-12-15 08:51:34 -0600

    defaults / events

--- docs/js/js.js
+++ docs/js/js.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
     var settings = {
-        width: 11,
-        height: 11,
-        bands: 4,
-        bandPct: 50,
+        width: 10,
+        height: 10,
+        bands: 3,
+        bandPct: 75,
         mono: true,
         alpha: false
     };
@@ -13,23 +13,10 @@ $(document).ready(function() {
         matrix[i] = new Array(settings.height);
     }
 
+    var dataURL = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAYAAACNMs+9AAAAPElEQVQoU2NkIBIwEqmOgfGZre1/ZMVShw/DxUBsmBzxCmE6YCbjNJEqCmGGgGyBOxab1VgVEgomosMRAKwjMAtoqEGrAAAAAElFTkSuQmCC";
+
     var patcanvas = $('#patcanvas')[0];
     var reflectCanvas = $('#reflectcanvas')[0];
-
-    init = function() {
-        randomMatrix();
-    };
-
-    setSettings = function() {
-        $.each($('#control-panel').serializeArray(), function(i, field) {
-            settings[field.name] = parseInt(field.value);
-        });
-
-        settings.mono = $('#control-panel #mono')[0].checked ? true : false;
-        settings.alpha = $('#control-panel #alpha')[0].checked ? true : false;
-
-        init();
-    };
 
     getRandomIntInclusive = function(min, max) {
         min = Math.ceil(min);
@@ -37,12 +24,27 @@ $(document).ready(function() {
         return Math.floor(Math.random() * (max - min + 1)) + min;
     }
 
-    randomMatrix = function() {
+    setSettings = function(e) {
+        e.preventDefault();
+        $.each($('#control-panel').serializeArray(), function(i, field) {
+            settings[field.name] = parseInt(field.value);
+        });
+
+        settings.mono = $('#control-panel #mono')[0].checked ? true : false;
+        settings.alpha = $('#control-panel #alpha')[0].checked ? true : false;
+
+        setMatrix();
+        randomMatrix();
+    };
+
+    setMatrix = function() {
         matrix = new Array(settings.width);
         for (var i = 0; i < matrix.length; i++) {
             matrix[i] = new Array(settings.height);
         }
+    };
 
+    randomMatrix = function() {
         for (var i = 0; i < matrix.length; i++) {
             for (var j = 0; j < matrix[i].length; j++) {
                 var rValue = new Array(4);
@@ -56,10 +58,10 @@ $(document).ready(function() {
                 matrix[i][j] = rValue;
             }
         }
-        redrawPreview();
+        drawPattern();
     };
 
-    redrawPreview = function() {
+    drawPattern = function() {
         patcanvas.width = settings.width;
         patcanvas.height = settings.height;
         reflectcanvas.width = patcanvas.width * 2;
@@ -88,18 +90,25 @@ $(document).ready(function() {
         reflectcontext.scale(1, -1);
         reflectcontext.drawImage(reflectcanvas, 0, 0);
 
+        dataURL = reflectcanvas.toDataURL("image/png");
+
+        setDOM();
+    };
+
+    setDOM = function() {
+
         $("#pattern-area").empty();
 
         for (i = 0; i < settings.bands; i++) {
             $("#pattern-area").append('<div class="band-container"><div class="band-positioner"><div class="band"></div></div></div>');
         }
 
-        var dataURL = reflectcanvas.toDataURL("image/png");
         $('.band').css('background-image', 'url(' + dataURL + ')');
         $('.band').css('height', settings.bandPct + '%');
     };
 
     $('#control-panel').on('change', setSettings);
+    $('#refresh').on('click', setSettings);
 
-    init();
+    setDOM();
 });
