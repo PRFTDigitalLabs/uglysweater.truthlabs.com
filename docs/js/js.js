b93e3b588b0e8b4f53880fb349fc26fc45ccbc6a
sconstantinides <sconstantinides@gmail.com>
2016-12-22 11:08:30 -0600

    remove form

--- docs/js/js.js
+++ docs/js/js.js
@@ -1,10 +1,17 @@
 $(document).ready(function() {
-    var settings = {},
+    var settings = {
+            width: 6,
+            height: 6,
+            bands: 4,
+            colors: 3,
+            empty: 25
+        },
         dataURL = [],
         patcanvas = [],
         reflectcanvas = [],
-        colors,
-        paletteOptions = ['tol-dv', 'tol-sq', 'tol-rainbow', 'rainbow'];
+        colorPalette = [];
+        paletteOptions = ['tol-dv', 'tol-sq', 'tol-rainbow'],
+        firstRun = true;
 
     patcanvas[1] = $('#patcanvas1')[0];
     patcanvas[2] = $('#patcanvas2')[0];
@@ -26,27 +33,19 @@ $(document).ready(function() {
         } : null;
     };
 
-    changeSettings = function(e) {
-        e.preventDefault();
-
+    knit = function() {
         setup();
-
         randomizePattern(1);
         randomizePattern(2);
         setDOM();
     };
 
     setup = function() {
-        $.each($('#control-panel').serializeArray(), function(i, field) {
-            settings[field.name] = parseInt(field.value);
-        });
-
-        settings.gray = $('#control-panel #gray')[0].checked ? true : false;
-
-        if (settings.gray) {
-            colorPalette = ["ffffff", "E63D3D"];
+        if (firstRun) {
+            colorPalette[1] = ["ffffff", "E63D3D"];
         } else {
-            colorPalette = palette(paletteOptions[Math.floor(Math.random() * paletteOptions.length)], settings.colors);
+            colorPalette[1] = palette(paletteOptions[Math.floor(Math.random() * paletteOptions.length)], settings.colors);
+            colorPalette[2] = palette(paletteOptions[Math.floor(Math.random() * paletteOptions.length)], settings.colors);
         }
     };
 
@@ -57,7 +56,7 @@ $(document).ready(function() {
             matrix[i] = [];
 
             for (var j = 0; j < settings.height; j++) {
-                var RGB = hexToRgb(colorPalette[Math.floor(Math.random() * colorPalette.length)]);
+                var RGB = hexToRgb(colorPalette[num][Math.floor(Math.random() * colorPalette[num].length)]);
                 var alpha = Math.random() >= (settings.empty / 100) ? 0 : 1;
 
                 matrix[i][j] = [RGB.r, RGB.g, RGB.b, alpha];
@@ -99,10 +98,11 @@ $(document).ready(function() {
     };
 
     setDOM = function() {
-
         $("#pattern-area").empty();
 
-        for (i = 0; i < settings.bands; i++) {
+        var numBands = firstRun ? settings.bands + 1 : settings.bands;
+
+        for (i = 0; i < numBands; i++) {
             $("#pattern-area").append('<div class="band-container"><div class="band-positioner"><img class="band"><img class="band"><img class="band"><img class="band"><img class="band"><img class="band"><img class="band"></div></div>');
         }
 
@@ -110,11 +110,11 @@ $(document).ready(function() {
         $('.band-container:nth-of-type(even) .band').attr('src', dataURL[2]);
     };
 
-    $('#control-panel').on('change', changeSettings);
-    $('#refresh').on('click', changeSettings);
-
     setup();
     randomizePattern(1);
     dataURL[2] = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAYAAACNMs+9AAAAPElEQVQoU2NkIBIwEqmOgfGZre1/ZMVShw/DxUBsmBzxCmE6YCbjNJEqCmGGgGyBOxab1VgVEgomosMRAKwjMAtoqEGrAAAAAElFTkSuQmCC";
     setDOM();
+    firstRun = false;
+
+    $('#knit').on('click', knit);
 });
